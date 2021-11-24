97a95a504da15778cfff484dfeb295281a3c9661
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-11-24 23:08:19 +0900

    #1 modify title page

--- energyfake/lib/res/theme.dart
+++ energyfake/lib/res/theme.dart
@@ -14,7 +14,7 @@ ThemeData _buildTheme() {
         style: ElevatedButton.styleFrom().copyWith(
             backgroundColor: MaterialStateProperty.all<Color>(buttonNormal))),
     buttonBarTheme: base.buttonBarTheme.copyWith(
-      buttonTextTheme: ButtonTextTheme.accent,
+      buttonTextTheme: ButtonTextTheme.primary,
     ),
     textTheme: _buildTextTheme(base.textTheme),
     primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
@@ -26,31 +26,30 @@ ThemeData _buildTheme() {
       },
     ),
     colorScheme: ColorScheme.fromSwatch().copyWith(secondary: accentColor),
-    appBarTheme: const AppBarTheme(
-        systemOverlayStyle: SystemUiOverlayStyle.light
-    ),
+    appBarTheme:
+        const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light),
   );
 }
 
 TextTheme _buildTextTheme(TextTheme base) {
   return base
       .copyWith(
-    headline5: base.headline5!.copyWith(
-      fontWeight: FontWeight.w500,
-    ),
-    headline6: base.headline6!.copyWith(fontSize: 18.0),
-    caption: base.caption!.copyWith(
-      fontWeight: FontWeight.w400,
-      fontSize: 14.0,
-    ),
-    bodyText1: base.bodyText1!.copyWith(
-      fontWeight: FontWeight.w500,
-      fontSize: 16.0,
-    ),
-  )
+        headline5: base.headline5!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        headline6: base.headline6!.copyWith(fontSize: 18.0),
+        caption: base.caption!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyText1: base.bodyText1!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
       .apply(
-    fontFamily: 'Rubik',
-    displayColor: mainBlack,
-    bodyColor: mainBlack,
-  );
+        fontFamily: 'Rubik',
+        displayColor: mainBlack,
+        bodyColor: mainBlack,
+      );
 }
