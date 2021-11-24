8e2beff68653db0b6b2bedecc5a71bc9b868ef15
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-11-24 17:20:30 +0900

    #1 localize this file

--- energyfake/lib/ui/home.dart
+++ energyfake/lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'navigator_pages.dart';
 import '../res/theme.dart';
@@ -9,6 +10,9 @@ class EnergyFake extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: themeData,
       home: const NavigatorPages(),
     );
