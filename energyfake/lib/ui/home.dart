7c58322fccc914cda57f34401d18d9792f63d6f2
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-11-24 10:56:58 +0900

    #1 Add theme

--- energyfake/lib/ui/home.dart
+++ energyfake/lib/ui/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'navigator_pages.dart';
+import '../res/colors.dart';
 
 class EnergyFake extends StatelessWidget {
   const EnergyFake({Key? key}) : super(key: key);
@@ -8,9 +10,61 @@ class EnergyFake extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: NavigatorPages(),
+    return MaterialApp(
+      theme: _themeData,
+      home: const NavigatorPages(),
     );
   }
+}
+
+final ThemeData _themeData = _buildTheme();
+
+ThemeData _buildTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    primaryColor: mainBlack,
+    scaffoldBackgroundColor: background,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom().copyWith(
+            backgroundColor: MaterialStateProperty.all<Color>(buttonNormal))),
+    buttonBarTheme: base.buttonBarTheme.copyWith(
+      buttonTextTheme: ButtonTextTheme.accent,
+    ),
+    textTheme: _buildTextTheme(base.textTheme),
+    primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    pageTransitionsTheme: const PageTransitionsTheme(
+      //ここを追加
+      builders: <TargetPlatform, PageTransitionsBuilder>{
+        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      },
+    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: accentColor),
+    appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.light
+    ),
+  );
+}
+
+TextTheme _buildTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+    headline5: base.headline5!.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    headline6: base.headline6!.copyWith(fontSize: 18.0),
+    caption: base.caption!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    bodyText1: base.bodyText1!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  )
+      .apply(
+    fontFamily: 'Rubik',
+    displayColor: mainBlack,
+    bodyColor: mainBlack,
+  );
 }
