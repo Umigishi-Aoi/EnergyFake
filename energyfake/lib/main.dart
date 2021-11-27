e53c1c6648a0dc650550419e69f83d0a51e44444
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-11-27 11:21:36 +0900

    #27 add function to hide home indicator

--- energyfake/lib/main.dart
+++ energyfake/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'ui/home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.immersive,
+    overlays: <SystemUiOverlay>[],
+  );
   runApp(const ProviderScope(child: EnergyFake()));
 }
